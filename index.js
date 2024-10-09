c7b57d4ac1caab73e8518167002d1af1493c73b3
gurpreetsinghsectiond <n01717631@humber.ca>
2024-10-09 09:11:49 -0400

    did

---
+++ index.js
@@ -0,0 +1,59 @@
+//TASK1 
+
+// 1. Declare a variable named myNumber and assign it the value of 10:
+
+let myNumber = 10;
+
+
+// 2. Declare another variable named myString and assign it the value of "Hello, JavaScript!":
+
+let myString = "Hello, JavaScript!";
+
+
+// 3. Print both variables to the console using console.log():
+
+console.log(myNumber);
+console.log(myString);
+
+
+//TASK2 FOR LOOP
+
+// 1. For loop from 0 to 10 and print the number:
+
+for (let i = 0; i <=10; i++) {
+console.log(i);    
+}
+
+// 2. For loop to print the first 5 multiples of 3:
+
+for (let i = 1; i <= 5; i++){
+console.log(3 * i);
+}
+
+
+//TASK3 
+
+// 1,2.  Define the addNumbers function and inside it and num1 and num2 together
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+
+// 2. Call the addNumbers function:
+
+const sum = addNumbers(7, 4)  
+console.log(sum); 
+
+
+// 3. Define the multiplyNumbers function:
+
+function multiplyNumbers(num1, num2){
+    return num1 * num2;
+}
+
+// 4. Call the multiplyNumbers function:
+
+let result = multiplyNumbers(3, 8)
+console.log(result) ; 
+
+
+
